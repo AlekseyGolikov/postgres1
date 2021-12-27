61e82465b03b528c8b8871ca3728bdc8c21ff435
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2021-12-27 17:35:49 +0500

    second

--- postgres.py
+++ postgres.py
@@ -3,13 +3,20 @@ from psycopg2 import Error
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 try:
-    with psycopg2.connect(user='user', password='12345', host='localhost', port='5432', database='test') as connection:
-        pass
+    with psycopg2.connect(user='postgres', password='12345', host='localhost', port='5432', database='test') as connection:
         connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         with connection.cursor() as cursor:
-            cursor.execute('SELECT * FROM users;')
-            record = cursor.fetchall()
-            print(f'Result: {record}')
+            insert_query = """INSERT INTO books (nick_name,author,num_lists,year) VALUES ('aaa','bbb',100,2021);"""
+            cursor.execute(insert_query)
+            connection.commit()
+            print('Note is done successfully!')
+
+            select_query = """SELECT * FROM books;"""
+            cursor.execute(select_query)
+            result = cursor.fetchall()
+            print(f'Result = {result}')
+
+            print()
 
 except (Exception, Error) as error:
     print(f'ERROR during PostgreSQL transaction {error}')
