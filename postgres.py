64b652ae8e7ba66e076d675cd40324aacd36b4ad
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2021-12-29 06:01:05 +0300

    SELECT, INSERT and UPDATE is done

--- postgres.py
+++ postgres.py
@@ -6,10 +6,14 @@ try:
     with psycopg2.connect(user='postgres', password='12345', host='localhost', port='5432', database='test') as connection:
         connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         with connection.cursor() as cursor:
-            insert_query = """INSERT INTO books (nick_name,author,num_lists,year) VALUES ('name2','author2',220,2021);"""
-            cursor.execute(insert_query)
-            connection.commit()
-            print('Note is done successfully!')
+            # insert_query = """INSERT INTO books (nick_name,author,num_lists,year) VALUES ('name2','author2',220,2021);"""
+            # cursor.execute(insert_query)
+            # connection.commit()
+            # print('Note is done successfully!')
+
+            # update_query = """UPDATE books SET nick_name = 'name1', author = 'author1' WHERE nick_name = 'name2' AND author = 'author2';"""
+            # cursor.execute(update_query)
+            # print('Note has been updated!')
 
             select_query = """SELECT * FROM books;"""
             cursor.execute(select_query)
